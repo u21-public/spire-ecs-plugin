9e170d230f9a01c27c4ceb337bb1a646e7eb929a
Jay Crumb <jay.crumb@unit21.ai>
2022-05-18 14:49:42 -0600

    Fix tagging, add plugin server
    With this, we can finally build the plugin for installation alongside
    our SPIRE agent. The label format doesn't require "ecs" as a prefix as
    SPIRE will automatically do that based on the plugin name in the
    agent configuration file.

---
+++ cmd/agent/ecs_attestor.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/spiffe/spire-plugin-sdk/pluginmain"
+	workloadattestorv1 "github.com/spiffe/spire-plugin-sdk/proto/spire/plugin/agent/workloadattestor/v1"
+	"github.com/u21/spire-ecs-plugin/pkg/agent"
+)
+
+func main() {
+	plugin := agent.NewECSWorkloadAttestor()
+
+	pluginmain.Serve(
+		workloadattestorv1.WorkloadAttestorPluginServer(plugin),
+	)
+}
--- pkg/agent/attestor.go
+++ pkg/agent/attestor.go
@@ -2,6 +2,7 @@ package agent
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/hashicorp/go-hclog"
 	"github.com/spiffe/spire-plugin-sdk/pluginsdk"
@@ -21,7 +22,7 @@ type Plugin struct {
 	metadataProvider metadataProvider
 }
 
-func New() *Plugin {
+func NewECSWorkloadAttestor() *Plugin {
 	return &Plugin{
 		metadataProvider: &ecsMetadataProvider{},
 	}
@@ -36,5 +37,49 @@ func (p *Plugin) SetMetadataProvider(provider metadataProvider) {
 }
 
 func (p *Plugin) Attest(context context.Context, _ *workloadattestorv1.AttestRequest) (*workloadattestorv1.AttestResponse, error) {
-	return nil, status.Error(codes.Unimplemented, "Not implemented")
+	metadata, err := p.metadataProvider.getMetadata()
+	if err != nil {
+		p.logger.Error("error fetching ecs metadata:", err)
+		return &workloadattestorv1.AttestResponse{}, status.Error(codes.Unavailable, "metadata failed to fetch")
+	}
+
+	return &workloadattestorv1.AttestResponse{
+		SelectorValues: p.getSelectorValuesFromMetadata(metadata),
+	}, nil
+}
+
+func (p *Plugin) getSelectorValuesFromMetadata(meta metadataResponse) []string {
+	type selector struct{ key, value string }
+
+	var selectorValues []string
+
+	if meta.IsEmpty {
+		return selectorValues
+	}
+
+	selectors := []selector{
+		{"name", meta.Root.Name},
+		{"image", meta.Root.Image},
+		{"cluster", meta.Task.Cluster},
+		{"arn", meta.Task.TaskARN},
+		{"family", meta.Task.Family},
+		{"revision", meta.Task.Revision},
+		{"availabilityzone", meta.Task.AvailabilityZone},
+	}
+
+	// Following the pattern of the existing docker plugin, labels
+	// will end up in the form ecs:label:some_label:some_value
+	for label, value := range meta.Root.Labels {
+		labelValue := fmt.Sprintf("%s:%s", label, value)
+		selectors = append(selectors, selector{"label", labelValue})
+	}
+
+	for _, s := range selectors {
+		selectorValues = append(
+			selectorValues,
+			fmt.Sprintf("%s:%s", s.key, s.value),
+		)
+	}
+
+	return selectorValues
 }
